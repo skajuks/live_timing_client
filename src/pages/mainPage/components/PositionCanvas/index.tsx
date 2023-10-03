acd319bcb7a659d8cb3ba4e0b5c096d45cecdc47
RVeips <rihardsv@na.lv>
2023-10-03 11:40:59 +0300

    react canvas component for position sim initial

---
+++ src/pages/mainPage/components/PositionCanvas/index.tsx
@@ -0,0 +1,88 @@
+import React, { useRef, useEffect } from "react"
+
+const UPDATE_RATE = 60
+
+
+function RenderPositionGraph(ctx: CanvasRenderingContext2D) {
+    const W = ctx.canvas.width
+    const H = ctx.canvas.height
+
+    ctx.clearRect(0, 0, W, H)
+
+    ctx.strokeStyle = "#F00"
+    ctx.lineWidth = 4
+    ctx.strokeRect(0, 0, W, H)
+}
+
+export const PositionCanvas: React.FC = () => {
+    const canvas_ref = useRef<HTMLCanvasElement>(null)
+    useEffect(() => {
+        let force_redraw = true
+        let force_resize = true
+
+        const canvas = canvas_ref.current as HTMLCanvasElement
+        const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+        ctx.imageSmoothingEnabled = false
+
+        let W: number
+        let H: number
+
+        const draw = () => {
+            force_redraw = false
+
+            requestAnimationFrame(() => {
+                RenderPositionGraph(ctx)
+            })
+        }
+
+        const resize = () => {
+            const bound = canvas.getBoundingClientRect()
+            if (!force_resize && (canvas.width == bound.width * window.devicePixelRatio) && (canvas.height == bound.height * window.devicePixelRatio))
+                return
+
+            force_resize = false
+
+            canvas.width = bound.width * window.devicePixelRatio
+            canvas.height = bound.height * window.devicePixelRatio
+            W = canvas.width
+            H = canvas.height
+
+            // UpdateSize(ctx)
+            ctx.clearRect(0, 0, W, H)
+
+            force_redraw = true
+            draw()
+        }
+
+
+        const rso = new ResizeObserver(resize)
+        rso.observe(canvas_ref.current as HTMLElement)
+
+        window.addEventListener("resize", resize)
+        resize()
+
+        const draw_interval = setInterval(draw, 1000 / UPDATE_RATE)
+
+        return () => {
+            rso.disconnect()
+            window.removeEventListener("resize", resize)
+            clearInterval(draw_interval)
+        }
+    }, [canvas_ref])
+
+    return <canvas ref={canvas_ref}
+        style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+        }}
+    />
+}
+
+
+export const PositionCanvas_Test: React.FC = () => {
+    return <div style={{ position: "relative", width: "800px", height: "200px", marginLeft: "auto", marginRight: "auto" }}><PositionCanvas /></div>
+}
+
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -4,6 +4,7 @@ import { SelectedLiveRace } from "./components/selectedLive/selectedLiveRace";
 import { SelectedLiveFull } from "./components/selectedLiveFull/selectedLiveFull";
 import { RaceHistoryGroups } from './components/history/raceHistoryGroups';
 import { RaceHistoryEvent } from "./components/history/event/raceHistoryEvent";
+import { PositionCanvas, PositionCanvas_Test } from "./components/PositionCanvas";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
 export const MainPage = () => {
@@ -26,7 +27,7 @@ export const MainPage = () => {
                 </ul>
             </nav>
             <div className="app__navbar-infobox">
-                This page is currently running in beta version, please be aware of any occoring bugs.
+                This page is currently running in beta version, please be aware of any occurring bugs.
             </div>
             <Routes>
                 <Route path={"/"} element={<LiveList />} />
@@ -34,8 +35,9 @@ export const MainPage = () => {
                 <Route path={"/history/:hash"} element={<RaceHistoryEvent />} />
                 <Route path={"/history/:hash/:eventId"} />
                 <Route path={"/live/"} />
-                <Route path={"/live/:hash"} element={<SelectedLiveRace />}/>
-                <Route path={"/live/full/:hash"} element={<SelectedLiveFull />}/>
+                <Route path={"/live/:hash"} element={<SelectedLiveRace />} />
+                <Route path={"/live/full/:hash"} element={<SelectedLiveFull />} />
+                <Route path={"/test"} element={<PositionCanvas_Test />} />
             </Routes>
             <footer className="app__live-footer">
                 This is a footer
